6ea23569da10d3e325fdaab7fb22d6fb72a23d72
Amin <Amin@DESKTOP-78DOJ82>
2022-02-03 12:36:52 +0600

    PHP notice fiexed

--- wordpress-paypal-shopping-cart/includes/admin/wp_shopping_cart_menu_addons.php
+++ wordpress-paypal-shopping-cart/includes/admin/wp_shopping_cart_menu_addons.php
@@ -4,16 +4,17 @@ function wspsc_show_addons_menu_page() {
     if (!current_user_can('manage_options')) {
         wp_die('You do not have permission to access this settings page.');
     }
+    $output = "";
 
     echo '<div class="wrap">';
     echo '<h1>' . (__("Simple Cart Add-ons", "wordpress-simple-paypal-shopping-cart")) . '</h1>';
-    
+
     echo '<div id="poststuff"><div id="post-body">';
     ?>
 
     <div class="wspsc_yellow_box">
         <p><?php _e("For more information, updates, detailed documentation and video tutorial, please visit:", "wordpress-simple-paypal-shopping-cart"); ?><br />
-            <a href="https://www.tipsandtricks-hq.com/wordpress-simple-paypal-shopping-cart-plugin-768" target="_blank"><?php _e("WP Simple Cart Homepage", "wordpress-simple-paypal-shopping-cart"); ?></a></p>
+            <a href="https://www.tipsandtricks-hq.com/ecommerce/wp-shopping-cart" target="_blank"><?php _e("WP Simple Cart Documentation", "wordpress-simple-paypal-shopping-cart"); ?></a></p>
     </div>
 
     <?php
@@ -34,7 +35,7 @@ function wspsc_show_addons_menu_page() {
         "page_url"	 => "https://www.tipsandtricks-hq.com/ecommerce/wp-shopping-cart-and-mailchimp-integration-3442",
     );
     array_push( $addons_data, $addon_2 );
-    
+
     $addon_3 = array(
         "name"		 => __( "WP Affiliate Plugin", 'wordpress-simple-paypal-shopping-cart' ),
         "thumbnail"	 => WP_CART_URL . "/includes/admin/images/wp-affiliate-plugin-integration.png",
@@ -42,7 +43,7 @@ function wspsc_show_addons_menu_page() {
         "page_url"	 => "https://www.tipsandtricks-hq.com/wordpress-affiliate-platform-plugin-simple-affiliate-program-for-wordpress-blogsite-1474",
     );
     array_push( $addons_data, $addon_3 );
-    
+
     /* Show the addons list */
     foreach ( $addons_data as $addon ) {
         $output .= '<div class="wspsc_addon_item_canvas">';
@@ -71,9 +72,9 @@ function wspsc_show_addons_menu_page() {
         $output .= '</div>'; //end canvas
     }
 
-    echo $output;    
-                
+    echo $output;
+
     echo '</div></div>';//End of poststuff and post-body
     echo '</div>';//End of wrap
-    
+
 }
