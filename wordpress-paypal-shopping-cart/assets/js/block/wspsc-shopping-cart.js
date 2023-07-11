d7f0a512f1e2d2b1f1a598578fbdc2c87b8977a2
adilrabid <rabid1819@gmail.com>
2023-07-11 16:37:57 +0600

    key prop warning issue fixed

--- wordpress-paypal-shopping-cart/assets/js/block/wspsc-shopping-cart.js
+++ wordpress-paypal-shopping-cart/assets/js/block/wspsc-shopping-cart.js
@@ -18,36 +18,37 @@ wspsc_register_block_type(
                 wspsc_element(
                     wspsc_serverSideRender,
                     {
+                        key: 'wspsc-shopping-cart-block-serverSideRenderer_key', // unique key.
                         block: wspsc_sc_block_block_meta.name,
                         attributes: props.attributes,
                     }
                 ),
 
-
                 wspsc_element(
                     wspsc_inspector_controls,
-                    null,
-                    [
+                    {
+                        key: "wspsc-shopping-cart-block-inspectorControl-key", // unique key.
+                    },
+                    wspsc_element(
+                        'div',
+                        {
+                            key: "wspsc-shopping-cart-block-div-key", // unique key.
+                            style: {padding: "16px",}
+                        },
                         wspsc_element(
-                            'div',
+                            wspsc_select_control,
                             {
-                                style: {padding: "16px",}
-                            },
-
-                            wspsc_element(
-                                wspsc_select_control,
-                                {
-                                    label: wspsc_sc_block_display_option_meta.label,
-                                    value: props.attributes.display_option,
-                                    help: wspsc_sc_block_display_option_meta.description,
-                                    options: wspsc_sc_block_display_option_meta.options,
-                                    onChange: (value) => {
-                                        props.setAttributes({display_option: value});
+                                key: "wspsc-shopping-cart-block-selectControl-key", // unique key.
+                                label: wspsc_sc_block_display_option_meta.label,
+                                value: props.attributes.display_option,
+                                help: wspsc_sc_block_display_option_meta.description,
+                                options: wspsc_sc_block_display_option_meta.options,
+                                onChange: (value) => {
+                                    props.setAttributes({display_option: value});
                                     },
                                 }
                             ),
                         ),
-                    ]
                 ),
             ];
         },
