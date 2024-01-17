55b65aef2016c23d8df230e41c45f3fdb6eb0a92
adilrabid <rabid1819@gmail.com>
2024-01-17 03:45:53 -0800

    PayPal PPCP post processing - code refractor, email and affiliate done

--- wordpress-paypal-shopping-cart/lib/paypal/class-tthq-paypal-utility-ipn-related.php
+++ wordpress-paypal-shopping-cart/lib/paypal/class-tthq-paypal-utility-ipn-related.php
@@ -189,11 +189,13 @@ class PayPal_Utility_IPN_Related {
 		// Save the transaction data.
 		PayPal_Utility_Functions::log( 'Saving transaction data to the database.', true );
 
-		\WPSC_Post_Payment_Related::save_txn_record( $ipn_data );
+		$processed_ipn_data = \WPSC_Post_Payment_Related::process_ipn_data( $ipn_data );
 
-		// \WPSC_Post_Payment_Related::send_notification_email();
+		\WPSC_Post_Payment_Related::save_txn_record( $processed_ipn_data );
 
-		// \WPSC_Post_Payment_Related::affiliate_plugin_integration();
+		\WPSC_Post_Payment_Related::send_notification_email($processed_ipn_data);
+
+		\WPSC_Post_Payment_Related::affiliate_plugin_integration($processed_ipn_data);
 
 		//FIXME - dispatch sale notification email.
 		//PayPal_Utility_Functions::dispatch_sale_notification_email( $ipn_data );
